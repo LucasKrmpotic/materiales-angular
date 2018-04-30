d3ddee6f44b22a1879beb92dbcee63c0fad3733a
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2018-04-30 12:47:46 -0300

    5ta seccion

---
+++ 5-seccion/pipes/src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: string, activar: boolean = true): any {
+
+    if (activar) { return value.replace(/\w/g, '*'); }
+    return value;
+
+  }
+}
